b15244f1d87034017199274ab3806655c257f830
Jason <155787583+ZZZhaoz@users.noreply.github.com>
2025-11-26 13:37:19 -0500

    Clean up comments in model.glsl
    Removed comments regarding model transformation matrix.

--- Code/src/model.glsl
+++ Code/src/model.glsl
@@ -1,12 +1,3 @@
-﻿// Construct the model transformation matrix. The moon should orbit around the
-// origin. The other object should stay still.
-//
-// Inputs:
-//   is_moon  whether we're considering the moon
-//   time  seconds on animation clock
-// Returns affine model transformation as 4x4 matrix
-//
-// expects: identity, rotate_about_y, translate, PI
 uniform bool is_background;
 
 mat4 rotate_about_x(float a)
